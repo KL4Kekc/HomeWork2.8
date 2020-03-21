d1f236ca3282b85f39ae55b133b50e39321e63f8
KL4Kekc <KL4.Kekc95kg@gmail.com>
2020-03-21 21:39:01 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<String, String[]> dictionary = new HashMap<String, String[]>();
+        dictionary.put("красивый", new String[]{"симпотичный", "румяный", "привлекательный", "прекрасный"});
+        dictionary.put("человек", new String[]{"существо", "личность", "персона", "индивидум"});
+        dictionary.put("дом", new String[]{"жилище", "помещение", "логово", "кров"});
+        dictionary.put("большой", new String[]{"огромный", "массивный", "крупный", "громадный"});
+
+        HashMap<String, String[]> bigDictionary = new HashMap<String, String[]>(dictionary);
+
+        Set<String> stringSet = dictionary.keySet();
+        Iterator<String> iterator = stringSet.iterator();
+
+        while (iterator.hasNext()) {
+            try {
+                String key = iterator.next();
+                String[] array = bigDictionary.get(key);
+                bigDictionary.put(key, array);
+
+                for (String str : array) {
+                    ArrayList<String> arrayList = new ArrayList<String>(array.length);
+                    arrayList.addAll(Arrays.asList(array));
+                    arrayList.add(key);
+                    arrayList.remove(str);
+                    String[] array1 = new String[arrayList.size()];
+                    array1 = arrayList.toArray(array1);
+                    bigDictionary.put(str, array1);
+                }
+            } catch (Exception ex) {
+            }
+        }
+
+        while (true) {
+            System.out.println("Введите слово: ");
+            Scanner scanner = new Scanner(System.in);
+            String sentences = scanner.nextLine();
+            String[] words = sentences.split(" ");
+            for (int i = 0; i < words.length; i++) {
+                if (bigDictionary.get(words[i]) == null) {
+                    System.out.println("Такого слова нет");
+                } else {
+                    String[] synonims = bigDictionary.get(words[i]);
+                    Random random = new Random();
+                    int randomIndex = random.nextInt(synonims.length);
+                    System.out.println(synonims[randomIndex] + " ");
+                }
+            }
+        }
+    }
+}
